b628f204caa09c2ace1bee5c04239686cc99e6a3
sumij95 <33921846+sumij95@users.noreply.github.com>
2017-11-27 21:44:36 +0000

    new tasks

--- test.php
+++ test.php
@@ -1,57 +1,9 @@
-<?php
-require_once "Mail.php";
+<div>
+    <li>ID change </li>
+    <li>pdf creation</li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
 
-$from = '<seym45@gmail.com>';
-$to = '<seym45@yahoo.com>';
-$subject = 'Hi!';
-$body = "Hi,\n\nHow are you?";
-
-$headers = array(
-    'From' => $from,
-    'To' => $to,
-    'Subject' => $subject
-);
-
-$smtp = Mail::factory('smtp', array(
-        'host' => 'ssl://smtp.gmail.com',
-        'port' => '465',
-        'auth' => true,
-        'username' => 'seym45@gmail.com',
-        'password' => '45AMARnam le'
-    ));
-
-$mail = $smtp->send($to, $headers, $body);
-
-if (PEAR::isError($mail)) {
-    echo('<p>' . $mail->getMessage() . '</p>');
-} else {
-    echo('<p>Message successfully sent!</p>');
-}
-
-?>
-<div id="content">
-     <h3>Hello, this is a H3 tag</h3>
-
-    <p>a pararaph</p>
 </div>
-<div id="editor"></div>
-<button id="cmd">Generate PDF</button>
-
-<script>
-    var doc = new jsPDF();
-    var specialElementHandlers = {
-        '#editor': function (element, renderer) {
-            return true;
-        }
-    };
-
-    $('#cmd').click(function () {
-        doc.fromHTML($('#content').html(), 15, 15, {
-            'width': 170,
-                'elementHandlers': specialElementHandlers
-        });
-        doc.save('sample-file.pdf');
-    });
-
-    // This code is collected but useful, click below to jsfiddle link.
-</script>
